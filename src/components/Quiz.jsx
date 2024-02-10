e9f52b55051ca840600b41212e30578f12e260e8
ankita-singh27 <rajputanki5@gmail.com>
2024-02-10 11:33:08 +0530

    timer added

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,23 +1,48 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import Loading from "./Loading";
 import Result from "./Result";
+import QuizHeader from "./QuizHeader";
 
 const Quiz = () => {
-  const navigate = useNavigate();
   const [questions, setQuestions] = useState([]);
   const [answers, setAnswers] = useState({});
   const [showResult, setShowResult] = useState(false);
   const [score, setScore] = useState(0);
   const [loading, setLoading] = useState(false);
+  const [timer, setTimer] = useState(60); 
+  const [timerIntervalId, setTimerIntervalId] = useState(null);
   const [status, setStatus] = useState("");
+
+  // Utility function to format time
+const formatTime = (seconds) => {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  const formattedTime = `${String(minutes).padStart(2, "0")}:${String(remainingSeconds).padStart(2, "0")}`;
+  return formattedTime;
+};
 
   useEffect(() => {
     fetch("/quiz.json")
       .then((response) => response.json())
       .then((data) => setQuestions(data))
       .catch((error) => console.error("Error fetching quiz data:", error));
-  }, []);
+
+      // Set up the timer interval
+    const intervalId = setInterval(() => {
+      setTimer((prevTimer) => {
+        // Check if the timer is greater than 0 before decrementing
+        return prevTimer > 0 ? prevTimer - 1 : prevTimer;
+      });
+    }, 1000);
+    setTimerIntervalId(intervalId);
+ 
+    return () => {
+      clearInterval(intervalId);
+      if (timer <= 0) {
+        setShowResult(true);
+      }
+    };
+  }, [timer]);
 
   const handleAnswerSelect = (questionId, selectedOption) => {
     // Handle answer selection logic here
@@ -29,6 +54,8 @@ const Quiz = () => {
   const handleSubmit = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
     setLoading(true);
+
+    clearInterval(timerIntervalId);
     // Calculate score and show result
     setTimeout(() => {
       const quizScore = calculateScore(answers);
@@ -60,11 +87,13 @@ const Quiz = () => {
     setScore(0);
     setShowResult(false);
     setLoading(false);
+    setTimer(60);
     //navigate("/quiz");
   };
 
   return (
     <section>
+     <QuizHeader timer={timer} formatTime={formatTime}/>
       <div className="md:w-9/12 w-[90%] flex md:flex-row flex-col mx-auto">
         {/* question section */}
         <div className="md:w-[70%] w-full">
---
+++ src/components/QuizHeader.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/prop-types */
+
+const QuizHeader = ({timer,formatTime}) => {
+    
+  return (
+    <section className="shadow-sm my-5 py-2  bg-white z-10 " >
+      <div className="w-9/12 mx-auto flex md:flex-row flex-col justify-between items-center">
+        <div className="font-normal">
+          <span className="text-red-700">Attention!</span> You have 60 seconds to
+          answer 6 questions.
+          <br />
+          Please keep an eye on the timer and make sure to answer all questions
+          before time runs out.
+        </div>
+
+        <div className="flex flex-col items-center">
+          {/* <div className="mr-2 text-xl text-gray-700">
+            <i className="fa-solid fa-clock-rotate-left"></i>
+          </div> */}
+            <h1 className="text-green-700 text-xl">
+              {formatTime(timer)}
+              <sub className="text-xs ml-1">sec</sub>
+            </h1>
+            <p className="text-xs -mt-1 text-gray-700">Time Consumed</p>
+          </div>
+
+        </div>
+    </section>
+  );
+};
+
+
+export default QuizHeader;
